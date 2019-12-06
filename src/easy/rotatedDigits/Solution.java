4bd95b362fdd8fd432904322efb0320278cbe912
Dizent <master@dizent.cn>
2019-12-06 10:25:03 +0800

    xinzuodetimu

---
+++ src/easy/rotatedDigits/Solution.java
@@ -0,0 +1,30 @@
+package easy.rotatedDigits;
+
+public class Solution {
+
+    public int rotatedDigits(int N) {
+        int[] goodArr = new int[]{2,2,1,0,0,1,1,0,2,1};
+        int count = 0;
+        for(int i = 1;i <= N;i++){
+            int occu = i;
+            boolean occuCount = false;
+            while (occu >= 1){
+                int num = occu % 10;
+                if(goodArr[num] == 0){
+                    occuCount = false;
+                    break;
+                }
+                if(goodArr [num] == 1){
+                    occuCount = true;
+                }
+                occu /= 10;
+            }
+            count += occuCount?1:0;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().rotatedDigits(200));
+    }
+}
